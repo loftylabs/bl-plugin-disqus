95f170c7c4982d34e67d66f2f3678696b7fb1c06
Casey Kinsey <casey.kinsey@gmail.com>
2015-12-17 14:37:55 -0600

    Adds template logic for plugin and plugin metadata file

---
+++ bl_plugin_disqus/plugin.py
@@ -0,0 +1,7 @@
+__plugin_meta__ = {
+    'author': "Casey Kinsey",
+    'title': "Baseline Disqus",
+    'url': "https://github.com/loftylabs/bl-plugin-disqus/",
+    'version': "0.1a",
+    'description': "Add disqus comments to blog posts.",
+}
